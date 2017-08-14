2eea28f775e9004b8e1e445bbe6037dc6d96a608
danielo515 <rdanielo@gmail.com>
2017-08-14 12:16:16 +0200

    feat(json): Added extract functionality

--- src/components/Extract.vue
+++ src/components/Extract.vue
@@ -1,22 +1,44 @@
 <template>
   <div>
-    <q-btn icon="create">Extract fields</q-btn>
+    <q-select
+      v-model="select"
+      float-label="Keys to include"
+     :options="keys"
+    />
+    <q-btn icon="create" @click="extract">Extract fields</q-btn>
   </div>
 </template>
 
 <script>
 import {
   QInput,
+  QSelect,
   QBtn
 } from 'quasar'
+
+import { mapState } from 'vuex'
+import Pick from 'lodash.pick'
 
 export default {
   components: {
     QBtn,
+    QSelect,
     QInput
   },
+  computed: mapState({
+    keys: state => state.keys.map(k => ({ label: k, value: k })),
+    input: state => state.jsonInput
+  }),
+  methods: {
+    extract() {
+      const result = this.input.map(o => Pick(o, this.select));
+      this.$store.dispatch('setJsonOutput', result)
+    }
+  },
   data () {
-    return {}
+    return {
+      select: ''
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -8,11 +8,23 @@ export const actions = {
   setJsonInput({commit}, value) {
     commit('SET_JSON_INPUT', value);
     commit('EXTRACT_KEYS');
+  },
+  /**
+   * Processing components should dispatch this action with the result of their processing of the JSON-input.
+   * The payload must be an object that will be formatted according to the configuration
+   * @param {*} param0 Set by vuex
+   * @param {Object} obj Should be an object representing the result of processing the input
+   */
+  setJsonOutput({commit, state}, obj) {
+    commit('SET_JSON_OUTPUT', JSON.stringify(obj, null, state.config.space))
   }
 };
 const mutations = {
   SET_JSON_INPUT(state, value) {
     state.jsonInput = value;
+  },
+  SET_JSON_OUTPUT(state, value) {
+    state.jsonOutput = value;
   },
   EXTRACT_KEYS(state) {
     const keys = listAllKeys(state.jsonInput);
@@ -25,7 +37,11 @@ export default new Vuex.Store({
   actions,
   mutations,
   state: {
+    config: {
+      space: 2
+    },
     jsonInput: [{}],
+    jsonOutput: [{}],
     keys: []
   }
 });
